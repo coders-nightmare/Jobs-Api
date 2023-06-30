7e35f72379b6a020218ce47794d5cadcbb11533f
Ritik <ritik09052000@gmail.com>
2023-06-30 12:29:56 +0530

    added login functionality with token authentication

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -5,7 +5,8 @@ const getJob = async (req, res) => {
   res.send("get Job");
 };
 const createJob = async (req, res) => {
-  res.send("create Job");
+  // res.send("job created");
+  res.json(req.user);
 };
 const updateJob = async (req, res) => {
   res.send("update Job");
